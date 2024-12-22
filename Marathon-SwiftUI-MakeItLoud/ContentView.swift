ad5479ae55970405601ac8daa72c0f6d13c41229
Sergey Leontiev <translatertrans@gmail.com>
2024-12-22 12:19:37 +0100

    Implement task

---
+++ Marathon-SwiftUI-MakeItLoud/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  Marathon-SwiftUI-MakeItLoud
+//
+//  Created by Sergey Leontiev on 21.12.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .blur(radius: 5, opaque: true)
+                .ignoresSafeArea()
+                
+            VolumeView()
+                .frame(width: 100, height: 225)
+                .frame(maxHeight: .infinity)
+        }
+      
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct VolumeView: View {
+    @State var progress: CGFloat = 0.2
+    @State var offset: CGFloat = 0
+    @State var lastOffset: Double = 0
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+            
+            ZStack(alignment: .bottom) {
+                Rectangle()
+                    .background(.ultraThinMaterial)
+                Rectangle()
+                    .fill(.white)
+                    .frame(height: max(progress, 0) * height)
+            }
+            .clipShape(.rect(cornerRadius: 30))
+            .frame(height: progress < 0 ? height - (progress * height) : nil)
+            .gesture (
+                DragGesture()
+                    .onChanged { value in
+                        offset = -value.translation.height + lastOffset
+                        calculateProgress(height: height)
+                    }
+                    .onEnded { _ in
+                        withAnimation(.smooth) {
+                            offset = min(height, max(offset, 0))
+                            calculateProgress(height: height)
+                        }
+                        lastOffset = offset
+                    }
+            )
+            .frame(
+                width: width,
+                height: height,
+                alignment: progress > 0 ? .bottom : .top
+            )
+        }
+    }
+    
+    private func calculateProgress(height: CGFloat) {
+        let scale = 0.15
+        let topExcessOffset = height + (offset - height) * scale
+        let bottomExcessOffset = offset < 0 ? offset * scale : offset
+        progress = (offset > height ? topExcessOffset : bottomExcessOffset) / height
+    }
+}
+
+
+struct AnimationProperties {
+    var translation = 0.0
+    var verticalStretch = 1.0
+}
---
+++ Marathon-SwiftUI-MakeItLoud/Marathon_SwiftUI_MakeItLoudApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marathon_SwiftUI_MakeItLoudApp.swift
+//  Marathon-SwiftUI-MakeItLoud
+//
+//  Created by Sergey Leontiev on 21.12.24..
+//
+
+import SwiftUI
+
+@main
+struct Marathon_SwiftUI_MakeItLoudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
